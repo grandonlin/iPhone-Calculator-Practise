5de40d244105d887c8e128cdc131b311b07b9025
Grandon Lin <glin@grandons-macbook-pro.local>
2016-08-04 23:55:22 -0400

    Initial Commit

---
+++ iPhone-Calculator-Practise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iPhone-Calculator-Practise
+//
+//  Created by Grandon Lin on 2016-08-04.
+//  Copyright © 2016 Grandon Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
